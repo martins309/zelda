95917d733d086b645b6e3de5c9af45c93acd326b
Andrew Martins <andrew_omartins@yahoo.com>
2020-12-18 16:26:17 -0500

    these are the changes to the enemies and their weapons

--- weapons.py
+++ weapons.py
@@ -29,3 +29,24 @@ class Golden_Bow(Weapons):
         self.price = 50
     def __str__(self):
         return "Golden Bow"
+
+class Club(Weapons):
+    def __init(self):
+        self.power = 7
+        self.price = 12
+    def __str__(self):
+        return "Club"
+
+class Flail(Weapons):
+    def __init__(self):
+        self.power = 30
+        self.price = 30
+    def __str__(self):
+        return "Flail"
+
+class FireBreath(Weapons):
+    def __init__(self):
+        self.power = 50
+        self.price = 50
+    def __str__(self):
+        return "FireBreath"
